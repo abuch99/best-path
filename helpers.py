ea9df29d5056c24743c0448add2fc9ba30382c66
arnav <abuch99@gmail.com>
2020-04-18 13:19:54 +0530

    duration matrix made

--- helpers.py
+++ helpers.py
@@ -22,25 +22,41 @@ def geoloc(sendurl):
     return []
 
 def getjson(nodedict):
-    data=[]
+    data_origin=[]
+    data_dest=[]
     jsondata = {}
     for key in nodedict:
         temp={}
         if key == 'RGIA':
             temp['latitude']=nodedict[key][0]
             temp['longitude']= nodedict[key][1]
-            jsondata['destinations']=temp
+            data_dest.append(temp)
         else:
             temp['latitude']=nodedict[key][0]
             temp['longitude']=nodedict[key][1]
-            data.append(temp)
+            data_origin.append(temp)
 
-    jsondata['origins']=data
-    json.dumps(jsondata)
+    jsondata['origins']=data_origin
+    jsondata['destinations']=data_dest
+    jsondata['travelMode']='driving'
+    jsondata=json.dumps(jsondata)
     return jsondata
 
-def distanceMatrix(nodedict):
-    jsondata=getjson(nodedict)    
+def distanceMatrix(sendurl,nodedict):
+    jsondata=getjson(nodedict)   
+    r = requests.post(url=sendurl, data=jsondata)
+    try:
+        j=json.loads(r.text)
+        distances=[]
+        try:
+            loc = j['resourceSets'][0]['resources'][0]['results']
+            for item in loc:
+                distances.append(item['travelDuration'])
+            return(((distances)))
+        except ValueError:
+            print("Wrong")
+    except ValueError:
+        print("Wrong")
 
 def openFiles():
     with open(places, encoding='utf-8-sig') as fileplace:
--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,9 @@
 import urllib.parse
-from helpers import geoloc,openFiles,getjson
+from helpers import geoloc,openFiles,distanceMatrix
 from graph import Graph
 import configparser
-import json
+import json as JSON
+from os import path
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -14,16 +15,39 @@ nodes = data[0]
 edges = data[1]
 
 nodedict=dict()
+if not path.exists('dict.json'):
+    for item in nodes:
+        urllib.parse.quote(item)
+        url='http://dev.virtualearth.net/REST/v1/Locations?query='+item+'&key='+key
+        nodedict[item]=geoloc(url)
 
-for item in nodes:
-    urllib.parse.quote(item)
-    sendurl = 'http://dev.virtualearth.net/REST/v1/Locations?countryRegion=India&locality=Hyderabad&addressLine='+item+'&userLocation=17.387140,78.491684&key='+key
-    nodedict[item]=geoloc(sendurl)
+    json = JSON.dumps(nodedict)
+    f = open("dict.json","w")
+    f.write(json)
+    f.close()
+
+else:
+    with open('dict.json', 'r') as f:
+        nodedict=JSON.load(f)
 
 g=Graph()
 for item in edges:
     g.addEdge(item)
 
-jsondata = getjson(nodedict)
-json.dumps(jsondata)
-print(jsondata)
+if not path.exists('duration.json'):
+    sendurl='https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?key='+key
+    matrix=distanceMatrix(sendurl, nodedict)
+    durations={}
+    for name,time in zip(nodes, matrix):
+        durations[name]=time
+
+    json = JSON.dumps(durations)
+    f = open("duration.json","w")
+    f.write(json)
+    f.close()
+
+else:
+    with open('duration.json', 'r') as f:
+        durations=JSON.load(f)
+
+print(durations)
